2c0b88b4daa435d08fa42d1ebdc597bf87632994
Lukas Borke <lukas.borke@web.de>
2019-12-13 22:17:58 +0100

    update keras_gpu Docker run

--- python/keras_gpu_prog.py
+++ python/keras_gpu_prog.py
@@ -59,14 +59,16 @@ train_datagen=ImageDataGenerator(preprocessing_function=preprocess_input) #inclu
 
 # paperspace P4000/P5000
 # training_data_path = '/data/BigSetFull'
-training_data_path = '/data/small'
+# training_data_path = '/data/small'
 # training_data_path = '/data/medium'
+
+training_data_path = '/data/train'
 
 
 train_generator=train_datagen.flow_from_directory(training_data_path,
 	target_size=(224,224),
 	color_mode='rgb',
-	batch_size=32,
+	batch_size=64,
 	class_mode='categorical',
 	shuffle=True)
 
@@ -74,6 +76,7 @@ train_generator=train_datagen.flow_from_directory(training_data_path,
 model.compile(optimizer='Adam',loss='categorical_crossentropy',metrics=['accuracy'])
 
 step_size_train = train_generator.n//train_generator.batch_size
+step_size_train
 
 
 model.fit_generator(generator=train_generator,
@@ -81,8 +84,8 @@ model.fit_generator(generator=train_generator,
 	# use_multiprocessing=True,
 	# workers = 8,
 	# epochs = 10)
-	# epochs = 2)
-	epochs = 30)
+	epochs = 2)
+	# epochs = 30)
 	# epochs = 100)
 
 
@@ -97,7 +100,7 @@ test_generator = test_datagen.flow_from_directory(
     directory='/data/test/',
     target_size=(224, 224),
     color_mode="rgb",
-    batch_size=1,
+    batch_size=32,
     class_mode=None,
     shuffle=False
 )
